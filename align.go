bad4b742047e4021d99cbbfe20cade3927cc0fb8
TerraTech <TerraTech@users.noreply.github.com>
2017-04-26 05:08:28 -0400

    Add Version() and underlying handling

---
+++ align.go
@@ -0,0 +1,10 @@
+// Copyright 2017 FutureQuest, Inc.
+
+package FQversion
+
+import (
+	"text/tabwriter"
+)
+
+func Align(io.Writer) {
+}
---
+++ interface.go
@@ -0,0 +1,7 @@
+// Copyright 2017 FutureQuest, Inc.
+
+package FQversion
+
+type Versioner interface {
+	Version() string
+}
