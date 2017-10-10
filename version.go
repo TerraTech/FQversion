d8703c4b90abc53ca7797b6d9e9a0bd5481e1671
TerraTech <TerraTech@users.noreply.github.com>
2017-10-10 16:09:46 -0700

    Overhaul the internal versioning system to use genVersion generator; implement the 'release' target

---
+++ version.go
@@ -0,0 +1,5 @@
+// Copyright 2017 FutureQuest, Inc.
+
+package FQversion
+
+//go:generate go run ../tools/genVersion.go -package $GOPACKAGE -prog "$PROG" -version "$VERSION" -build "$BUILD" -lib "$LIB"
